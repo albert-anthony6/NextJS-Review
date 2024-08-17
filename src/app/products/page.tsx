c5fa9583009a746e3fd6929c1cbc7aacee3cf577
Albert Valdes <albert.anthony1999@gmail.com>
2024-08-17 16:04:47 -0500

    link component basics

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,3 +1,21 @@
-export default function Products() {
-  return <h1>Product List</h1>;
+import Link from "next/link";
+
+export default function ProductsList() {
+  return (
+    <>
+      <h1>Product List</h1>
+      <Link href="/">Home</Link>
+      <h2>
+        <Link href="/products/1">Product 1</Link>
+      </h2>
+      <h2>
+        <Link href="/products/2">Product 2</Link>
+      </h2>
+      <h2>
+        <Link href="/products/3" replace>
+          Product 3
+        </Link>
+      </h2>
+    </>
+  );
 }
