21ba2988b34c8fd80b4e7e16e52b9896c0a21a59
Albert Valdes <albert.anthony1999@gmail.com>
2024-08-17 14:16:10 -0500

    layout file basics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,25 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <header
+          style={{
+            backgroundColor: "lightblue",
+            padding: "1rem",
+          }}
+        >
+          <p>Header</p>
+        </header>
+        {children}
+        <footer
+          style={{
+            backgroundColor: "ghostwhite",
+            padding: "1rem",
+          }}
+        >
+          <p>footer</p>
+        </footer>
+      </body>
     </html>
   );
 }
