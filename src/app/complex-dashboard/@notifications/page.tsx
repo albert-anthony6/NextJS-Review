947c356bdf3b2360d29adde97e18e0234de31886
Albert Valdes <albert.anthony1999@gmail.com>
2024-08-17 20:42:10 -0500

    unmatched routes basics

--- src/app/complex-dashboard/@notifications/page.tsx
+++ src/app/complex-dashboard/@notifications/page.tsx
@@ -1,5 +1,11 @@
 import Card from "@/components/Card";
+import Link from "next/link";
 
 export default function Notifications() {
-  return <Card>Notifications page</Card>;
+  return (
+    <Card>
+      <div>Notifications page</div>
+      <Link href="/complex-dashboard/archived">Archived</Link>
+    </Card>
+  );
 }
---
+++ src/app/complex-dashboard/@users/default.tsx
@@ -0,0 +1,5 @@
+import Card from "@/components/Card";
+
+export default function DefaultUsersAnalytics() {
+  return <Card>User Analytics</Card>;
+}
